012f66ff2f609afa696b74cfe5c83de247d53711
Tobias Lindström <me@tobiaslindstrom.se>
2022-09-27 16:56:41 +0200

    Trying to read e2e.toml and zip the files.

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require('@actions/core')
+const http = require('@actions/http-client')
+const exec = require('@actions/exec')
+const { readFile } = require('fs/promises')
 
+// TODO: Pack together the code? Test open the e2e.toml file.
+// TODO: Make sure that we send the Action version so that old ones can be rejected.
+// TODO: Throw error if not on Linux.
+
+const CONFIG_FILENAME = 'e2e.toml'
+
+const getConfigString = async () => {
+  try {
+    const configBuffer = await readFile(CONFIG_FILENAME)
+
+    return configBuffer.toString()
+  } catch (error) {
+    throw new Error(`E2E config file named "${CONFIG_FILENAME}" not found in current working directory.`)
+  }
+}
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    await getConfigString()
+    await exec.exec('zip -r e2e.zip . -x ".git/*" ".github/*"')
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    await exec.exec('ls -lah')
 
-    core.setOutput('time', new Date().toTimeString());
+    const client = new http.HttpClient('temp')
+
+    const res = await client.postJson('http://host.docker.internal:4444/hej', { temp: 1 })
+
+    console.log('res', res)
   } catch (error) {
+    console.log(error)
     core.setFailed(error.message);
   }
 }
