ad81964e8448bf886a7b58807515b8b34cfb0c09
Tobias Lindström <me@tobiaslindstrom.se>
2022-10-03 15:18:34 +0200

    Added tests for the main E2E run.

--- src/requestClient.js
+++ src/requestClient.js
@@ -1,10 +1,32 @@
-const createRun = async (apiKey) => {
+const http = require('@actions/http-client')
 
+const client = new http.HttpClient('e2e-tool-action')
+
+const createRun = async (apiKey) => {
+  const url = process.env.BASE_URL || 'http://host.docker.internal:4444'
+
+  const { result } = await client.postJSON(`${url}/runs`, {
+    apiKey,
+    actionVersion: '0.0.1',
+  })
+
+  return result
 }
+
+const uploadRunZip = async (runId, runZip) => {
+  const url = process.env.BASE_URL || 'http://host.docker.internal:4444'
+  const zipStream = runZip.createReadStream()
+
+  await client.sendStream('POST', `${url}/stream/${runId}`, zipStream)
+}
+
+// TODO: Implement with runId argument.
 const pollRunStatus = async () => {
 
 }
+
 module.exports = {
   createRun,
+  uploadRunZip,
   pollRunStatus,
 }
